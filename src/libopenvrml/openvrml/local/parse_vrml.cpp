0c232808f87e23452d4e633c038f2e4345ba9001
Dimitry Kloper <kloper@users.sf.net>
2013-04-05 00:26:14 +0300

    Remove dgd initialization, it will be invoked from boxfish

--- src/libopenvrml/openvrml/local/parse_vrml.cpp
+++ src/libopenvrml/openvrml/local/parse_vrml.cpp
@@ -126,14 +126,6 @@ parse_vrml(std::istream & in,
     typedef multi_pass<istreambuf_iterator<char> > multi_pass_iterator_t;
     typedef istream::char_type char_t;
 
-    if(dgd::controller<char>::get() == NULL) {
-        std::vector<std::string> args;
-        args.push_back("openvrml");
-        args.push_back("--trace-log=openvrml.log");
-        args.push_back("--trace-max-width=200");
-        dgd::controller<char>::init(args);
-    }
-
     vrml97_skip_grammar skip_g;
 
     if (iequals(type, vrml_media_type) || iequals(type, x_vrml_media_type)) {
